5c834c78b534db87ae9fc14f7de33c58f210c090
Serhii <ks.sergey.ko@gmail.com>
2022-06-23 13:53:00 +0300

    Add database connection

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ from forms import LoginForm, RegisterForm, AddTransactionForm
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SECRET_KEY'] = 'my_secret_key'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+# app.config['SECRET_KEY'] = 'my_secret_key'
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI",  "sqlite:///cashlogger.db")
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI",  "sqlite:///cashlogger.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
